f8704312868c8bfe84c9a2ae2e4a2a74a115429c
Theo Akesseh Sarpong <theo.sarp@yahoo.com>
2024-05-07 15:50:50 -0700

    Done

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Queries a given reddit API and prints the titles of the first 10 hot posts
+"""
+import requests
+from sys import argv
+
+
+def top_ten(subreddit):
+    """
+        Prints the titles of top ten posts for a given subreddit
+    """
+    user = {'User-Agent': 'Lizzie'}
+    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
+                       .format(subreddit), headers=user).json()
+    try:
+        for post in url.get('data').get('children'):
+            print(post.get('data').get('title'))
+    except Exception:
+        print(None)
+
+
+if __name__ == "__main__":
+    top_ten(argv[1])
